97e14ffd73f6e68d8d52abb0c98aec8fc1189ba8
mariia_sokolova_2 <mariasokolova0195@gmail.com>
2024-02-23 14:37:08 +0300

    MovieListItem and MovieDetail have been created

--- app/src/main/java/com/popularmovieapp/MainActivity.kt
+++ app/src/main/java/com/popularmovieapp/MainActivity.kt
@@ -3,29 +3,166 @@ package com.popularmovieapp
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.popularmovieapp.ui.MainScreen
-import com.popularmovieapp.ui.theme.PopularMovieAppTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-                MainScreen()
+            MainScreen()
         }
     }
 }
 
 
 //lazy column item
-
-
+@Preview
+@Composable
+fun MovieListItem() {
+    Card(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+        shape = RoundedCornerShape(15.dp),
+        elevation = CardDefaults.cardElevation(
+            defaultElevation = 5.dp
+        )
+    ) {
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(Color.White)
+        ) {
+            Row(
+                Modifier.padding(10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_foreground),
+                    contentDescription = "movie poster small",
+                    modifier = Modifier
+                        .padding(5.dp)
+                        .size(64.dp)
+                        .clip(RectangleShape)
+                )
+                Column(
+                    Modifier
+                        .padding(10.dp)
+                        .weight(weight = 1F, fill = true)
+                ) {
+                    Text(text = "Movie Name Rus", fontSize = 20.sp, fontWeight = FontWeight(800))
+                    Text(text = "Movie Name Eng", fontSize = 15.sp)
+                }
+                Text(
+                    text = "7.2",
+                    fontSize = 24.sp,
+                    fontWeight = FontWeight(800),
+                    modifier = Modifier.padding(16.dp)
+                )
+            }
+        }
+    }
+}
 
 
 //movie detail screen
+@Preview
+@Composable
+fun MovieDetail() {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)
+            .padding(16.dp)
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_background),
+            contentDescription = "movie poster big",
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(400.dp)
+                .clip(RectangleShape)
+        )
+        Row(modifier = Modifier.fillMaxWidth()) {
+            Column(
+                modifier = Modifier
+                    .weight(weight = 1F, fill = true)
+            ) {
+                Text(
+                    text = "Movie Name Eng",
+                    fontSize = 14.sp,
+                    color = Color.Gray,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 16.dp)
+                )
+                Text(
+                    text = "Movie Name Rus",
+                    fontSize = 25.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.Bold,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                )
+                Text(
+                    text = "genre, genre, genre",
+                    fontSize = 18.sp,
+                    color = Color.Gray,
+                    fontWeight = FontWeight.Medium,
+                    textAlign = TextAlign.Center,
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                )
+            }
+            Text(
+                text = "7.2",
+                fontSize = 25.sp,
+                color = Color.Black,
+                fontWeight = FontWeight.Bold,
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(alignment = Alignment.CenterVertically)
+                    .padding(top = 8.dp)
+            )
+        }
+        Text(
+            text = "Description",
+            fontSize = 14.sp,
+            color = Color.Gray,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(top = 16.dp)
+        )
+
+    }
+}
